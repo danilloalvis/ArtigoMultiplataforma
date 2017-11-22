18f955da4c78c39c87efcdbe9b2e0a58183aab7f
Danilo Torquato <danilo.torquato@indexdigital.com.br>
2017-11-22 14:47:58 -0300

    primeiro commit

---
+++ MyTask - Xamarin/Droid/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using FFImageLoading.Forms;
+using FFImageLoading.Forms.Droid;
+using System.Reflection;
+
+namespace MyTask.Droid
+{
+	[Activity(Label = "MyTask.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			LoadApplication(new App());
+		}
+	}
+}
---
+++ MyTask - Xamarin/MyTask/Views/Task.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace MyTask
+{
+
+
+    public partial class Tasks : ContentPage
+    {
+        Del gesture;
+        public Tasks(object o)
+        {
+            gesture = (Del)o;
+
+            InitializeComponent();
+            Debug.WriteLine("App Fim: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+
+            init();
+
+        }
+
+
+
+
+        private void init()
+        {
+
+            GetPlacesAsync();
+
+
+            myList.ItemSelected += OnMenuItemSelected;
+
+        }
+
+
+        private async void OnMenuItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            try
+            {
+                var item = (Task)e.SelectedItem;
+                if (item != null)
+                {
+                    Debug.WriteLine("Navegação de Paginas Inicio: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+                    await this.Navigation.PushAsync(new Details(item, gesture));
+
+
+                }
+                myList.SelectedItem = null;
+            }
+            catch
+            {
+            }
+
+        }
+
+
+        public async void GetPlacesAsync()
+        {
+            progress.IsRunning = true;
+            progress.IsVisible = true;
+            progress.IsEnabled = true;
+            myList.IsVisible = false;
+
+            var client = new HttpClient();
+            string placesJson = "";
+            try
+            {
+                Debug.WriteLine("Conectar Inicio: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+                var response = await client.GetAsync("http://mdta-com-br.umbler.net/api");
+                Debug.WriteLine("Conectar Fim: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+                placesJson = response.Content.ReadAsStringAsync().Result;
+            }
+            catch
+            {
+
+            }
+
+
+
+
+            //Debug.WriteLine(placesJson);
+
+            if (placesJson != null && !placesJson.Equals(""))
+            {
+                Debug.WriteLine("Json to object Inicio: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+                List<Task> list = JsonConvert.DeserializeObject<List<Task>>(placesJson);
+                Debug.WriteLine("Json to object Fim: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+
+                Debug.WriteLine("Object to json Inicio: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+                JsonConvert.SerializeObject(list);
+                Debug.WriteLine("Object to json Fim: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+
+                myList.ItemsSource = list;
+                progress.IsRunning = false;
+                progress.IsVisible = false;
+                progress.IsEnabled = false;
+                myList.IsVisible = true;
+
+                Debug.WriteLine("Armazenar Tarefas Inicio: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+                App.Current.Properties["list"] = placesJson;
+                Debug.WriteLine("Armazenar Tarefas Fim: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+
+            }
+            else
+            {
+                if (App.Current.Properties.ContainsKey("list"))
+                {
+                    Debug.WriteLine("Ler Tarefas Inicio: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+                    string result = (String)App.Current.Properties["list"];
+                    Debug.WriteLine("Ler Tarefas Fim: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff"));
+                    //Debug.WriteLine("---> " + result);
+
+                    if (result != null && !result.Equals(""))
+                    {
+                        List<Task> list = JsonConvert.DeserializeObject<List<Task>>(result);
+                        myList.ItemsSource = list;
+                    }
+
+                    progress.IsRunning = false;
+                    progress.IsVisible = false;
+                    myList.IsVisible = true;
+                }
+
+            }
+
+
+
+        }
+
+    }
+}
