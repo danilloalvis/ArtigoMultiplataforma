1fd18d882f793ce8a8d744e303a2aab7af62b18d
Danilo Torquato <danilo.torquato@indexdigital.com.br>
2017-11-22 15:10:18 -0300

    xamarin

---
+++ MyTask - Xamarin/MyTask/Views/Page1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MyTask
+{
+	public partial class Page1 : ContentPage
+	{
+		public Page1()
+		{
+			InitializeComponent();
+		}
+	}
+}
